bfa9de60b3a4e6ad305b95dbcde9c42af0b40b38
Gerard Said <gerard.said@mcast.edu.mt>
2020-12-01 16:15:05 +0100

    The problem was the foreach

--- customAIMoveScript.cs
+++ customAIMoveScript.cs
@@ -13,6 +13,12 @@ public class customAIMoveScript : MonoBehaviour
 
 
     public Transform target;
+
+    GameObject graphParent;
+
+    GameObject targetNode;
+
+    bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,95 +26,99 @@ public class customAIMoveScript : MonoBehaviour
 
         seeker = GetComponent<Seeker>();
 
-        seeker.StartPath(transform.position, target.position, ReadyToMove);
-    }
-    //when the path is generated this method is called.
-    void ReadyToMove(Path p)
-    {
-        Debug.Log("Path complete " + p.error);
-        pathToFollow = p;
-        StartCoroutine(moveTowardsPath());
+        targetNode = GameObject.FindGameObjectWithTag("targetnode");
+
+        graphParent = GameObject.Find("PointGraphObject");
+
+        graphParent.GetComponent<AstarPath>().Scan();
+
+        pathToFollow = seeker.StartPath(transform.position, target.position, pathCompleted);
+
+        StartCoroutine(moveTarget());
+
+        StartCoroutine(updateGraph());
+
+        StartCoroutine(moveTowardsPathAI(this.transform));
     }
 
-    IEnumerator moveTowardsPath()
+    //the code that is going to move my target.
+    IEnumerator moveTarget()
     {
-        foreach(Vector3 position in pathToFollow.vectorPath)
+        List<Vector3> positions = new List<Vector3>();
+        //target's current position
+        positions.Add(target.position);
+
+        positions.Add(new Vector3(target.position.x, -target.position.y));
+    
+        StartCoroutine(moveTowardsPath(target.transform, positions));
+
+        yield return null;
+
+    }
+
+    void pathCompleted(Path p)
+    {
+        pathToFollow = p;
+    }
+
+    IEnumerator updateGraph()
+    {
+        while(true)
+        { 
+            
+            targetNode.transform.position = target.position;
+            graphParent.GetComponent<AstarPath>().Scan();
+            
+            
+            yield return null;
+            
+        }
+
+    }
+
+    IEnumerator moveTowardsPath(Transform t, List<Vector3> points)
+    {
+        foreach (Vector3 position in points)
         {
-            while(Vector3.Distance(this.transform.position,position)>0.5f)
+            while (Vector3.Distance(t.position, position) > 0.5f)
             {
-                this.transform.position = Vector3.MoveTowards(this.transform.position, position, 1f);
+                t.position = Vector3.MoveTowards(t.position, position, 1f);
                 Debug.Log(position);/**/
-                yield return new WaitForSeconds(0.1f);
+                yield return new WaitForSeconds(0.2f);
             }
         }
         yield return null;
     }
 
-    //Task: can we give the robot a tail, just like we did with snake?
 
-
-
-/*
- *  void drawTail(int length)
-{
-    clearTail();
-
-    if (pastPositions.Count>length)
+    IEnumerator moveTowardsPathAI(Transform t)
     {
-        //nope
-        //I do have enough positions in the past positions list
-        //the first block behind the player
-        int tailStartIndex = pastPositions.Count - 1;
-        int tailEndIndex = tailStartIndex - length;
-
-
-        //if length = 4, this should give me the last 4 blocks
-        for (int snakeblocks = tailStartIndex;snakeblocks>tailEndIndex;snakeblocks--)
+        
+        while (true)
         {
-            //prints the past position and its order in the list
-            //Debug.Log(pastPositions[snakeblocks].Position + " " + pastPositions[snakeblocks].PositionOrder);
+            
+            List<Vector3> posns = pathToFollow.vectorPath;
+            for (int counter = 0; counter < posns.Count;counter++)
+            {
+                while(Vector3.Distance(t.position,posns[counter])>0.5f)
+                {
+                    t.position = Vector3.MoveTowards(t.position, posns[counter], 1f);
+                         
+                    pathToFollow = seeker.StartPath(transform.position, target.position, pathCompleted);
+                    yield return pathToFollow.IsDone();
+                    posns = pathToFollow.vectorPath;
+                    
+                    yield return new WaitForSeconds(0.2f);
+                }
 
-            Debug.Log(snakeblocks);
-
-            pastPositions[snakeblocks].BreadcrumbBox = Instantiate(breadcrumbBox, pastPositions[snakeblocks].Position, Quaternion.identity);
-            pastPositions[snakeblocks].BreadcrumbBox.GetComponent<SpriteRenderer>().color = snakeColor;
-
+            }
+            yield return null;
         }
-
-    } 
-
-    if (firstrun)
-    {
-
-        //I don't have enough positions in the past positions list
-        for(int count =length;count>0;count--)
-        {
-            positionRecord fakeBoxPos = new positionRecord();
-            float ycoord = count * -1;
-            fakeBoxPos.Position = new Vector3(0f, ycoord);
-           // Debug.Log(new Vector3(0f, ycoord));
-            //fakeBoxPos.BreadcrumbBox = Instantiate(breadcrumbBox, fakeBoxPos.Position, Quaternion.identity);
-            //fakeBoxPos.BreadcrumbBox.GetComponent<SpriteRenderer>().color = Color.yellow;
-            pastPositions.Add(fakeBoxPos);
-
-
-
-
-        }
-        firstrun = false;
-        drawTail(length);
-        //Debug.Log("Not long enough yet");
     }
 
-}
-
-
-*/
-
-// Update is called once per frame
 void Update()
 {
-
+       
 }
 }
 
